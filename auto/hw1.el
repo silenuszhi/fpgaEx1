ebd11e64794e9c9c95a6531f674175e31452b1bc
silenuszhi <silenuszhi@gmail.com>
2017-12-28 15:12:31 +0800

    更新.强调脚本

---
+++ auto/hw1.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook
+ "hw1"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "geometry"
+    "listings"
+    "fontspec"
+    "xeCJK"))
+ :latex)
+
